05c90c88833469656e3d206c5c5eb2fbe45e9289
Son NH <creativevoi@gmail.com>
2012-06-08 11:17:19 +0700

    update core class

---
+++ inc/admin.php
@@ -0,0 +1,6 @@
+<?php
+
+class WillKitAdmin extends WillKit
+{
+	
+}
--- index.php
+++ index.php
@@ -7,16 +7,241 @@ Author URI: http://creativevoi.com
 Version: 1.0
 */
 
-define('WILLKIT_PLUGIN_URL', plugin_dir_url( __FILE__ ));
-define('WILLKIT_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
-define('WILLKIT_TABLE', 'willdata');
+class WillKit
+{
+    var $table_willdata = 'willdata';
 
-require_once(WILLKIT_PLUGIN_PATH.'inc/core.php');
-require_once(WILLKIT_PLUGIN_PATH.'inc/helper.php');
-require_once(WILLKIT_PLUGIN_PATH.'ajax.php');
-require_once(WILLKIT_PLUGIN_PATH.'admin.php');
+    var $pluginPath = '';
+    var $pluginURL = '';
+    var $incPath = '';
+    var $version = '2.0';
 
-class WillKit extends CVCore 
+    function __construct()
+    {
+        $this->pluginPath = plugin_dir_path(__FILE__);
+        $this->pluginURL = plugin_dir_url(__FILE__);
+        $this->incPath = $this->pluginPath . 'inc/';
+    }
+
+/********** INITIALLY FUNCTIONS **********/
+
+    function init()
+    {
+        if (is_admin()) {
+            require_once($this->incPath . 'admin.php');
+            
+        } else {
+
+        }
+
+        register_activation_hook(__FILE__, array(&$this, 'on_activation'));
+        register_deactivation_hook(__FILE__, array(&$this, 'on_deactivation'));
+    }
+
+    function on_activation()
+    {
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+        $table = $this->db->prefix . $this->table_willdata;
+        if($this->db->get_var("SHOW TABLES LIKE '$table'") != $table) {
+            $sql = "CREATE TABLE /*!32312 IF NOT EXISTS*/ `$table` (
+                      `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                      PRIMARY KEY (`id`)
+                    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+                    ";
+            dbDelta($sql);
+        }
+    }
+    
+    function on_deactivation()
+    {
+    }
+
+/********** UTILITY FUNCTIONS **********/
+
+    function loadModel($name='', $shortname='')
+    {
+        $modelPath = $this->incPath . 'model_' . $name . '.php';
+        if ( !file_exists($modelPath) ) {
+            wp_die("File <strong>$modelPath</strong> does not exist.");
+        } else {
+            if ( property_exists($this, "table_$name") ) {
+                $tableName = $this->{"table_$name"};
+            } else {
+                wp_die("Property <strong>table_$name</strong> does not exist.");
+            }
+            require_once($this->incPath . 'model.php');
+            require_once($modelPath);
+            
+            $className = "model_$name";
+            
+            if ( !empty($shortname) ) {
+                $this->{$shortname} = new $className($tableName);
+            } else {
+                $this->{$name} = new $className($tableName);
+            }
+        }
+    }
+
+    function loadTpl($name='', $return=false)
+    {
+        $tplPath = $this->pluginPath . 'tpl/' . $name . '.php';
+        if ( !file_exists($tplPath) ) {
+            wp_die("File <strong>$tplPath</strong> does not exist.");
+        } else {
+            foreach ($this->data as $var=>$value) {
+                $$var = $value;
+            }
+            if ( !$return ) {
+                require($tplPath);
+            } else {
+                ob_start();
+                require($tplPath);
+                $html = ob_get_contents();
+                ob_end_clean();
+                return $html;
+            }
+        }
+    }
+    
+    function strip_request()
+    {
+        $ar = array();
+        foreach ($_GET as $key => $value)
+        {
+            if ( !in_array($key, array('p')) )
+                $ar[$key] = $value;
+        }
+        return $ar;
+    }
+    
+    function pagination($page = 1, $total = 0, $rows = 1, $is_ajax = FALSE)
+    {
+        $rows = intval($rows);
+        $page = intval($page);
+        $total = intval($total);
+        if ($total == 0) {
+            return array(
+                'text' => "Showing 0/0",
+                'goto_slb' => 'N/A',
+                'ipp_slb' => 'N/A',
+                'current_page' => 0,
+                'total_page' => 0,
+                'total_item' => 0,
+                'links' => ""
+            );
+        }
+        if ($rows <= 0)
+            $rows = 1;
+        $totalPages = ceil($total / $rows);
+        if ($page <= 0)
+            $page = 1;
+        if ($page > $totalPages)
+            $page = $totalPages;
+    
+        $sReturn = "";
+        $offset = 2;
+        $loop_num = $offset * 2 + 1;
+    
+        $params = '';
+        $ar_params = $this->strip_request();
+    
+        foreach ($ar_params as $key => $value) {
+            $params .= $key . '=' . urlencode($value) . '&amp;';
+        }
+        
+        $base_url = site_url($_SERVER['PHP_SELF']) . '?' . $params . 'p=';
+    
+        if ($page != 1) {
+            $href = $is_ajax ? "javascript:;" : $base_url . ($page - 1);
+            $sReturn .= "<a href='$href' title='Go to page " . ($page - 1) . "' p='" . ($page - 1) . "' class='page_numbers prev'><span>&laquo; Previous</span></a>";
+        }
+    
+        if ($totalPages < $loop_num + 4) {
+            // NOT MUCH PAGE
+            for ($i = 1; $i <= $totalPages; $i++) {
+                $href = $is_ajax ? "javascript:;" : $base_url . $i;
+                $sReturn .= ( $i == $page) ?
+                        "<span class='page_numbers current_page'>$i</span>" :
+                        "<a href='$href' title='Go to page $i' p='$i' class='page_numbers'><span>$i</span></a>";
+            }
+        } else {
+            // MUCH PAGE
+            if ($page < $loop_num) {
+                for ($i = 1; $i <= $loop_num; $i++) {
+                    $href = $is_ajax ? "javascript:;" : $base_url . $i;
+                    $sReturn .= ( $i == $page) ?
+                            "<span class='page_numbers current_page'>$i</span>" :
+                            "<a href='$href' title='Go to page $i' p='$i' class='page_numbers'><span>$i</span></a>";
+                }
+                $href = $is_ajax ? "javascript:;" : $base_url . $totalPages;
+                $sReturn .= "<span class='page_numbers dots'>...</span>"
+                        . "<a href='$href' title='Go to page $totalPages' p='$totalPages' class='page_numbers'><span>$totalPages</span></a>";
+            } else {
+                $href = $is_ajax ? "javascript:;" : $base_url . "1";
+                $sReturn .= "<a href='$href' title='Go to page 1' p='1' class='page_numbers'><span>1</span></a>"
+                        . "<span class='page_numbers dots'>...</span>";
+                if ($page > $totalPages - $loop_num + 1) {
+                    for ($i = $totalPages - $loop_num + 1; $i <= $totalPages; $i++) {
+                        $href = $is_ajax ? "javascript:;" : $base_url . $i;
+                        $sReturn .= ( $i == $page) ?
+                                "<span class='page_numbers current_page'>$i</span>" :
+                                "<a href='$href' title='Go to page $i' p='$i' class='page_numbers'><span>$i</span></a>";
+                    }
+                } else {
+                    for ($i = $page - $offset; $i <= $page + $offset; $i++) {
+                        $href = $is_ajax ? "javascript:;" : $base_url . $i;
+                        $sReturn .= ( $i == $page) ?
+                                "<span class='page_numbers current_page'>$i</span>" :
+                                "<a href='$href' title='Go to page $i' p='$i' class='page_numbers'><span>$i</span></a>";
+                    }
+                    $href = $is_ajax ? "javascript:;" : $base_url . $totalPages;
+                    $sReturn .= "<span class='page_numbers dots'>...</span>"
+                            . "<a href='$href' title='Go to page $totalPages' p='$totalPages' class='page_numbers'><span>$totalPages</span></a>";
+                }
+            }
+        }
+    
+        if ($page < $totalPages) {
+            $href = $is_ajax ? "javascript:;" : $base_url . ($page + 1);
+            $sReturn .= "<a href='$href' title='Go to page " . ($page + 1) . "' p='" . ($page + 1) . "' class='page_numbers next'><span>Next &raquo;</span></a>";
+        }
+    
+        $slb_goto = '<select name="p" class="gotoslb paginationslb">';
+        for ($i = 1; $i <= $totalPages; $i++) {
+            $slb_goto .= '<option value="' . $i . '"' . ($i == $page ? ' selected="selected"' : '') . '>' . $i . '</option>';
+        }
+        $slb_goto .= '</select>';
+    
+        $slb_itemperpage = '<select name="ipp" class="ippslb paginationslb">';
+        for ($i = 10; $i <= 100; $i+=10) {
+            $slb_itemperpage .= '<option value="' . $i . '"' . ($i == $rows ? ' selected="selected"' : '') . '>' . $i . '</option>';
+        }
+        $slb_itemperpage .= '</select>';
+    
+        return array(
+            'text' => "Showing item " . (($page - 1) * $rows + 1) . "-" . ($page * $rows < $total ? $page * $rows : $total) . " of " . number_format($total),
+            'goto_slb' => $slb_goto,
+            'ipp_slb' => $slb_itemperpage,
+            'current_page' => $page,
+            'total_page' => $totalPages,
+            'total_item' => $total,
+            'links' => $sReturn
+        );
+    }
+    
+}
+
+global $WillKit;
+$WillKit = new WillKit();
+$WillKit->init();
+
+// require_once(WILLKIT_PLUGIN_PATH.'inc/core.php');
+// require_once(WILLKIT_PLUGIN_PATH.'inc/helper.php');
+// require_once(WILLKIT_PLUGIN_PATH.'ajax.php');
+// require_once(WILLKIT_PLUGIN_PATH.'admin.php');
+
+class WillKit1 extends CVCore 
 {
     var $loadJsCss = false;
     
@@ -56,26 +281,4 @@ class WillKit extends CVCore
         
         $this->loadview('form', $this->data);
     }
-    
-    function on_activation()
-    {
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-        $table = $this->db->prefix . WILLKIT_TABLE;
-        if($this->db->get_var("SHOW TABLES LIKE '$table'") != $table) {
-            $sql = "CREATE TABLE /*!32312 IF NOT EXISTS*/ `$table` (
-                      `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                      PRIMARY KEY (`id`)
-                    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-                    ";
-            dbDelta($sql);
-        }
-    }
-    
-    function on_deactivation()
-    {
-        
-    }
 }
-
-$WillKit = new WillKit();
